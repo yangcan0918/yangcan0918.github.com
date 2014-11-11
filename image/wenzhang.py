06eab0924797c52b167eb350320e6e08a58814ab
yangcanyangcan <yangcan@yangcanyangcandeMac-Pro.local>
2014-11-11 11:34:33 +0800

    Site updated: 2014-11-11 11:34:30

---
+++ image/wenzhang.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#encoding: utf8
+import urllib2
+import urllib
+import re
+
+def getHtml(url):
+	return urllib2.urlopen(url).read()
+
+def getImg(html):
+	x=0	
+	reg=r'<p>(.+?)</p>'
+	imgre=re.compile(reg)
+	imglist=re.findall(imgre,html)
+	for imgurl in imglist:
+		print imgurl
+		#urllib.urlretrieve(imgurl,'meinv%d.jpg' %x)
+		x+=1
+		
+
+for i in range(1,2):
+	try: 
+		html= getHtml("http://web.toutiao.com/a3639750539/")
+		getImg(html)
+	except urllib2.HTTPError:
+		print "URL 不存在"
+
+
+
